984541d448b0839192812c10e059675ee93972b3
Nexdeys <deys2620@gmail.com>
2026-04-08 01:21:45 +0530

    Delete frontend directory

--- frontend/backend/src/routes/video.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/:id/preview', (req, res) => res.json({ message: 'Video preview' }));
-
-module.exports = router;
